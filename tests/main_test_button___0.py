f47a70df6b1508897f1744862604328f2ef29091
veles-gh <web.new.nd@gmail.com>
2024-12-24 12:13:36 +0300

    first test

---
+++ tests/main_test_button___0.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+import pytest
+
+import time
+
+
+url = f"https://www.qa-practice.com/elements/button/simple"
+
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+
+try:
+    time.sleep(4)
+    driver.get(url=url)
+
+    # assert driver.find_element(By.ID, 'submit-id-submit').is_displayed()
+    button_test = driver.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+    if button_test == True:
+        print(button_test)
+        print("Button found!!!")
+
+    else:
+        print(button_test)
+        print("Button not found...")
+
+except Exception as ex:
+    print(ex)
+
+
+finally:
+    time.sleep(3)
+    driver.close()
+    driver.quit()
+
+
+
